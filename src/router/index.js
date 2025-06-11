1cd80b5e6e21e97e8ad25a2ba0177a5b8f4841b5
jmillaa <millarra4@gmail.com>
2025-06-11 14:30:00 +0200

    empiezo con la parte del arbitro

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PantallaInicio from '../components/PantallaInicio.vue'
 import Marcador from '../components/Marcador.vue'
-import LoginArbitro from '../views/LoginArbitro.vue'
-import PanelArbitro from '../views/PanelArbitro.vue'
+import LoginArbitro from '../components/LoginArbitro.vue'
+//import PanelArbitro from '../components/PanelArbitro.vue'
 
 const routes = [
   { path: '/', component: PantallaInicio },
   { path: '/partido/:id', component: Marcador, props: true },
   { path: '/login-arbitro', component: LoginArbitro },
-  { path: '/panel-arbitro', component: PanelArbitro, props: route => ({ id: route.query.id }) },
+  //{ path: '/panel-arbitro', component: PanelArbitro, props: route => ({ id: route.query.id }) },
 ]
 
 
